ed6dad75ba7790b6a48f7bee23d1e98402524430
mosaek2 <mosaek2@gmail.com>
2024-09-14 03:57:52 +0900

    Ono2OneRepository, Ono2OneService, Ono2OneController error solved

--- src/main/java/com/team2/leopold/service/One2OneService.java
+++ src/main/java/com/team2/leopold/service/One2OneService.java
@@ -18,7 +18,10 @@ public class One2OneService {
 
     @Transactional
     public void insertPost(One2One one2One) {
-        repository.insertPosts(one2One);
+        //repository.insertPosts(one2One);
+        //코드에 문제가 있어서 주석처리 했습니다.
+        //엔티티로 데이터베이스에 정보를 추가하는 메소드는 save라는 기본적으로 제공해주는 메소드가 있습니다.
+        //UserService 파일을 참고해보세요.
     }
 
 }
