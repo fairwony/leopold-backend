141b3066844cefe6137b8ef0aefd9684f085f2b6
fairwony <fairwony@gmail.com>
2024-09-17 22:44:31 +0900

    추석

--- src/main/java/com/team2/leopold/service/NoticeService.java
+++ src/main/java/com/team2/leopold/service/NoticeService.java
@@ -1,4 +1,17 @@
 package com.team2.leopold.service;
 
+import com.team2.leopold.repository.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
 public class NoticeService {
+    private NoticeRepository repository;
+
+    @Autowired
+    public NoticeService(NoticeRepository noticeRepository) {
+        this.repository = noticeRepository;
+    }
 }
