dd0f41b90044689f83701fab7deb6512ace4fb01
fairwony <fairwony@gmail.com>
2024-09-19 15:22:27 +0900

    ResponseNoticeDto

--- src/main/java/com/team2/leopold/dto/ResponseNoticeDto.java
+++ src/main/java/com/team2/leopold/dto/ResponseNoticeDto.java
@@ -1,5 +1,59 @@
 package com.team2.leopold.dto;
 
-public class ResponseNoticeDto {
+import java.time.LocalDate;
 
+public class ResponseNoticeDto {
+    private Integer uid;
+    private String title;
+    private String name;
+    private LocalDate writeDate;
+    private Integer hit;
+
+    public ResponseNoticeDto(Integer uid, String title, String name, LocalDate writeDate, Integer hit) {
+        this.uid = uid;
+        this.title = title;
+        this.name = name;
+        this.writeDate = writeDate;
+        this.hit = hit;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getWriteDate() {
+        return writeDate;
+    }
+
+    public void setWriteDate(LocalDate writeDate) {
+        this.writeDate = writeDate;
+    }
+
+    public Integer getHit() {
+        return hit;
+    }
+
+    public void setHit(Integer hit) {
+        this.hit = hit;
+    }
 }
